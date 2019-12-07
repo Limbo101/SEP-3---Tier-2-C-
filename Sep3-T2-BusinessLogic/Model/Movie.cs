2fba2f89369c8be0faa2db73bd4a3518920e066b
Adrian Patrascu <280188@via.dk>
2019-12-07 15:12:53 +0100

    Api

---
+++ Sep3-T2-BusinessLogic/Model/Movie.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sep3_T2_BusinessLogic.Model
+{
+    public class Movie
+    {
+        public string Title { get; set; }
+        public string Cinema { get; set; }
+        public string Duration { get; set; }
+        public string StartHour { get; set; }
+    }
+}
---
+++ Sep3-T2-BusinessLogic/Model/Tier3Connection.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sep3_T2_BusinessLogic.Model
+{
+    public class Tier3Connection
+    {
+        public Movie GetMovie(string Date) 
+        {
+            //socket connection to tier 3 to receive details about movies from a specific date according to the movie class
+            return new Movie();
+        }
+
+    }
+}
